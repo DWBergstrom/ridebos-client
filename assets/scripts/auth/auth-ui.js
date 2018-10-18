003117d2012b04ceb63c661503dc21e026a18335
dwbergstrom <dwbergstrom@gmail.com>
2018-10-18 10:30:55 -0400

    Fix UI to hide ride content on sign out
    + update signOutSuccess function
    + adding hidden class back to ride content elements
    + clear ride info HTML

--- assets/scripts/auth/auth-ui.js
+++ assets/scripts/auth/auth-ui.js
@@ -3,31 +3,29 @@
 const store = require('../store.js')
 
 const signUpSuccess = function () {
-  $('#display-message').show()
-  $('#display-message').html('Sign-up successful! Please sign in.')
-  $('#display-message').css('color', 'green')
+  $('#auth-message').show()
+  $('#auth-message').html('Sign-up successful! Please sign in.')
+  $('#auth-message').css('color', 'green')
   $('#sign-up-form').trigger('reset')
 }
 
 const signUpError = function () {
-  $('#display-message').show()
-  $('#display-message').html('Something went wrong! Please try signing up again.').fadeOut(5000)
-  $('#display-message').css('color', 'red')
+  $('#auth-message').show()
+  $('#auth-message').html('Something went wrong! Please try signing up again.').fadeOut(5000)
+  $('#auth-message').css('color', 'red')
   $('#sign-up-form').trigger('reset')
 }
 
 const signInSuccess = function (response) {
-  $('#display-message').show()
-  $('#display-message').html('Sign-in successful!').fadeOut(5000)
-  $('#display-message').css('color', 'green')
+  $('#auth-message').show()
+  $('#auth-message').html('Sign-in successful!').fadeOut(5000)
+  $('#auth-message').css('color', 'green')
   $('#sign-in-form').trigger('reset')
   $('#change-password-form').trigger('reset')
-  $('#display-message2').show()
   store.user = response.user
   $('#sign-up-form').addClass('hidden')
   $('#sign-in-form').addClass('hidden')
   $('#change-password-form').removeClass('hidden')
-  $('.display-message2').removeClass('hidden')
   $('#sign-out-button').removeClass('hidden')
   $('#total-rides').removeClass('hidden')
   $('#ride-create').removeClass('hidden')
@@ -37,57 +35,50 @@ const signInSuccess = function (response) {
 }
 
 const signInError = function () {
-  $('#display-message').show()
-  $('#display-message').html('Something went wrong! Please try signing in again.').fadeOut(5000)
-  $('#display-message').css('color', 'red')
+  $('#auth-message').show()
+  $('#auth-message').html('Something went wrong! Please try signing in again.').fadeOut(5000)
+  $('#auth-message').css('color', 'red')
   $('#sign-in-form').trigger('reset')
 }
 
 const changePasswordSuccess = function () {
-  $('#password-message').show()
-  $('#password-message').html('Password change successful!').fadeOut(5000)
-  $('#password-message').css('color', 'green')
+  $('#auth-message').show()
+  $('#auth-message').html('Password change successful!').fadeOut(5000)
+  $('#auth-message').css('color', 'green')
   $('#sign-in-form').trigger('reset')
   $('#change-password-form').trigger('reset')
 }
 
 const changePasswordFailure = function () {
-  $('#password-message').show()
-  $('#password-message').html('Something went wrong with your password change! Please try again.').fadeOut(5000)
-  $('#password-message').css('color', 'red')
+  $('#auth-message').show()
+  $('#auth-message').html('Something went wrong with your password change! Please try again.').fadeOut(5000)
+  $('#auth-message').css('color', 'red')
   $('#sign-in-form').trigger('reset')
   $('#change-password-form').trigger('reset')
 }
 
 const signOutSuccess = function () {
-  $('#display-message').show()
-  $('#display-message').html('Sign out successful!').fadeOut(4000)
-  $('#display-message').css('color', 'green')
+  $('#ride-content').html('')
+  $('#auth-message').show()
+  $('#auth-message').html('Sign out successful!').fadeOut(4000)
+  $('#auth-message').css('color', 'green')
   $('#sign-in-form').trigger('reset')
-  $('#display-message').show()
+  $('#auth-message').show()
   $('#sign-up-form').removeClass('hidden')
   $('#sign-in-form').removeClass('hidden')
-  $('.display-message2').addClass('hidden')
   $('#change-password-form').addClass('hidden')
   $('#sign-out-button').addClass('hidden')
-  $('.game-container').addClass('hidden')
-  $('#reset-game-board').addClass('hidden')
-  $('.game-alert').addClass('hidden')
-  $('.game-details').addClass('hidden')
-  $('.game-stats').addClass('hidden')
-  $('#games-played').addClass('hidden')
-  $('.game-title').addClass('hidden')
-  $('.game-alert1').html('')
-  $('.game-alert2').html('')
-  $('.game-alert3').html('')
-  $('.game-alert4').html('')
-  $('.game-id').html('')
+  $('#total-rides').addClass('hidden')
+  $('#ride-create').addClass('hidden')
+  $('#ride-update').addClass('hidden')
+  $('#ride-destroy').addClass('hidden')
+  $('#one-ride').addClass('hidden')
 }
 
 const signOutFailure = function () {
-  $('#display-message').show()
-  $('#display-message').html('Something went wrong signing out! Check your network connection.')
-  $('#display-message').css('color', 'red')
+  $('#auth-message').show()
+  $('#auth-message').html('Something went wrong signing out! Check your network connection.')
+  $('#auth-message').css('color', 'red')
   $('#sign-in-form').trigger('reset')
 }
 
