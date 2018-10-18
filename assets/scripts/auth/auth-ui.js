27c610209205b606917a7915bfc7ce1288227424
dwbergstrom <dwbergstrom@gmail.com>
2018-10-18 10:30:55 -0400

    Added auth class to auth form elements
    ...to trigger form resets
    + Changed id based form resets to class based

--- assets/scripts/auth/auth-ui.js
+++ assets/scripts/auth/auth-ui.js
@@ -6,55 +6,47 @@ const signUpSuccess = function () {
   $('#auth-message').show()
   $('#auth-message').html('Sign-up successful! Please sign in.')
   $('#auth-message').css('color', 'green')
-  $('#sign-up-form').trigger('reset')
+  $('.auth').trigger('reset')
 }
 
 const signUpError = function () {
   $('#auth-message').show()
   $('#auth-message').html('Something went wrong! Please try signing up again.').fadeOut(5000)
   $('#auth-message').css('color', 'red')
-  $('#sign-up-form').trigger('reset')
+  $('.auth').trigger('reset')
 }
 
 const signInSuccess = function (response) {
   $('#auth-message').show()
   $('#auth-message').html('Sign-in successful!').fadeOut(5000)
   $('#auth-message').css('color', 'green')
-  $('#sign-in-form').trigger('reset')
-  $('#change-password-form').trigger('reset')
+  $('.auth').trigger('reset')
   store.user = response.user
-  $('#sign-up-form').addClass('hidden')
-  $('#sign-in-form').addClass('hidden')
+  $('.auth-in').addClass('hidden')
   $('#change-password-form').removeClass('hidden')
   $('#sign-out-button').removeClass('hidden')
-  $('#total-rides').removeClass('hidden')
-  $('#ride-create').removeClass('hidden')
-  $('#ride-update').removeClass('hidden')
-  $('#ride-destroy').removeClass('hidden')
-  $('#one-ride').removeClass('hidden')
+  $('.rides').removeClass('hidden')
 }
 
 const signInError = function () {
   $('#auth-message').show()
   $('#auth-message').html('Something went wrong! Please try signing in again.').fadeOut(5000)
   $('#auth-message').css('color', 'red')
-  $('#sign-in-form').trigger('reset')
+  $('.auth').trigger('reset')
 }
 
 const changePasswordSuccess = function () {
   $('#auth-message').show()
   $('#auth-message').html('Password change successful!').fadeOut(5000)
   $('#auth-message').css('color', 'green')
-  $('#sign-in-form').trigger('reset')
-  $('#change-password-form').trigger('reset')
+  $('.auth').trigger('reset')
 }
 
 const changePasswordFailure = function () {
   $('#auth-message').show()
   $('#auth-message').html('Something went wrong with your password change! Please try again.').fadeOut(5000)
   $('#auth-message').css('color', 'red')
-  $('#sign-in-form').trigger('reset')
-  $('#change-password-form').trigger('reset')
+  $('.auth').trigger('reset')
 }
 
 const signOutSuccess = function () {
@@ -62,24 +54,18 @@ const signOutSuccess = function () {
   $('#auth-message').show()
   $('#auth-message').html('Sign out successful!').fadeOut(4000)
   $('#auth-message').css('color', 'green')
-  $('#sign-in-form').trigger('reset')
+  $('.auth').trigger('reset')
   $('#auth-message').show()
-  $('#sign-up-form').removeClass('hidden')
-  $('#sign-in-form').removeClass('hidden')
-  $('#change-password-form').addClass('hidden')
-  $('#sign-out-button').addClass('hidden')
-  $('#total-rides').addClass('hidden')
-  $('#ride-create').addClass('hidden')
-  $('#ride-update').addClass('hidden')
-  $('#ride-destroy').addClass('hidden')
-  $('#one-ride').addClass('hidden')
+  $('.auth-in').removeClass('hidden')
+  $('.auth-out').addClass('hidden')
+  $('.rides').addClass('hidden')
 }
 
 const signOutFailure = function () {
   $('#auth-message').show()
   $('#auth-message').html('Something went wrong signing out! Check your network connection.')
   $('#auth-message').css('color', 'red')
-  $('#sign-in-form').trigger('reset')
+  $('.auth').trigger('reset')
 }
 
 module.exports = {
